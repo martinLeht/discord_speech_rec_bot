cb0974e8a4b1b06811ccefb5cd207affbc47cb6a
martinLeht <martin.lehtomaa@gmail.com>
2021-07-01 20:55:54 +0300

    Typescript node setup

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const PORT = 3002;
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+});
